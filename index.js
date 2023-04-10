0cbc58f9f283fc6b390d19b1d7b41c4e520cae5a
martscastrillo <mart.castrillo@gmail.com>
2023-04-10 23:27:08 +0200

    add final result

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 "use strict";
+
+let equal_pressed =  0;
+
+// refer all buttons excluding AC and DEL
+
+let button_input = document.querySelectorAll(".input_button");
+
+let input = document.getElementById("input");
+let equal = document.getElementById("equal");
+let clear = document.getElementById("clear");
+let erase = document.getElementById("erase");
+
+window.onload = () => {
+    input.value = "";
+};
+
+button_input.forEach((button_class) => {
+    button_class.addEventListener("click", ()=>{
+        if(equal_pressed ==1){
+            input.value= "";
+            equal_pressed = 0;
+        }
+        input.value += button_class.value;
+    })
+});
+equal.addEventListener("click", ()=>{
+    equal_pressed = 1;
+    let inp_val = input.value;
+    try {
+        let solution = eval (inp_val);
+        if(Number.isInteger(solution)){
+            input.value= solution
+        }
+        else{
+            input.value = solution.toFixed(2);
+        }
+      } catch (error) {
+       alert("Invalid Input");
+      }
+      
+});
+
+clear.addEventListener("click", () =>{
+    input.value = "";
+});
+
+erase.addEventListener("click", ()=>{
+    input.value = input.value.substr(0, input.value.length -1);
+})
